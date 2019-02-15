57ef2e3d5ff8e0699947b0e98a0c0d26aedca293
mwcookiesncream <47545578+mwcookiesncream@users.noreply.github.com>
2019-02-14 20:20:29 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 //analog sensors
 #define LIGHT 0
-
-
+int speed=200
+//maxis500
 // servo ports
 #define ELECTRIC 0
+int turnleft(){
+set_create_total_angle(0);
+create_drive_direct (0,speed);
+//left@,0speed
+while (get_create_angle()<90){}
+ create_stop();return 0;}
+
+int turnright(){
+set_create_total_angle(0);
+create_drive_direct (speed,0);
+//right@,0speed
+while (get_create_angle()<-90){}
+ create_stop();return 0;}
  
 main() {
 printf("Hello World.\n");
